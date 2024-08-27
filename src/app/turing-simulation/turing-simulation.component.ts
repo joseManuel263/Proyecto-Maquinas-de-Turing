bc99fd562b559b0c7685d5c1858e40bd3daa5e77
Miguel Angel Díaz Rivera <l.dirm040525@itses.edu.mx>
2024-08-26 20:05:19 -0600

    Correciones librerias

--- src/app/turing-simulation/turing-simulation.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-turing-simulation',
-  templateUrl: './turing-simulation.component.html'
-})
-export class TuringSimulationComponent {
-  @Input() results: string[] = [];
-}
